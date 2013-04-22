79e16b4b1cc6c1186f7b4cb4836cbd0c6f4bae01
ubergesundheit <ubergesundheit@googlemail.com>
2013-04-22 21:32:19 +0200

    added the fonts proposed by the marketing group

--- car.ioMockup/src/de/ifgi/car/iomockup/CheckListActivity.java
+++ car.ioMockup/src/de/ifgi/car/iomockup/CheckListActivity.java
@@ -1,10 +1,14 @@
 package de.ifgi.car.iomockup;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.app.SherlockActivity;
@@ -12,6 +16,29 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 
 public class CheckListActivity extends SherlockActivity implements OnClickListener {
+	
+	
+	public static final class TYPEFACE {
+	    public static final Typeface Raleway(Context ctx){
+	        Typeface typeface = Typeface.createFromAsset(ctx.getAssets(), "Raleway-Regular.ttf");
+	        return typeface;
+	    }
+	    public static final Typeface Newscycle(Context ctx){
+	        Typeface typeface = Typeface.createFromAsset(ctx.getAssets(), "newscycle_regular.ttf");
+	        return typeface;
+	    }
+	    public static void applyCustomFont(ViewGroup list, Typeface customTypeface) {
+            for (int i = 0; i < list.getChildCount(); i++) {
+                View view = list.getChildAt(i);
+                if (view instanceof ViewGroup) {
+                    applyCustomFont((ViewGroup) view, customTypeface);
+                } else if (view instanceof TextView) {
+                    ((TextView) view).setTypeface(customTypeface);
+                }
+            }
+        }	    
+	} 
+	
 
 	private ActionBar actionBar;
 
@@ -23,6 +50,11 @@ public class CheckListActivity extends SherlockActivity implements OnClickListen
 		actionBar = getSupportActionBar();
 		actionBar.setHomeButtonEnabled(true);
 		actionBar.setDisplayHomeAsUpEnabled(true);
+		
+		View rootView = findViewById(R.id.checklist_root_layout);
+		TYPEFACE.applyCustomFont((ViewGroup)rootView, TYPEFACE.Newscycle(this));
+		
+		((TextView) this.findViewById(R.id.welcome)).setTypeface(TYPEFACE.Raleway(this));
 		
 		this.findViewById(R.id.fourthItem).setOnClickListener(this);
 	}
