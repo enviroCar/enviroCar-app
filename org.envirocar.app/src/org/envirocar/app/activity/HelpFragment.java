0267406301ea4e9399b076b622d74df7f94e7e9d
Jakob Moellers <jakob.moellers@gmail.com>
2013-07-05 17:23:41 +0200

    started implementing the help page. no graphics yet. ugly layout.

--- org.envirocar.app/src/org/envirocar/app/activity/HelpFragment.java
+++ org.envirocar.app/src/org/envirocar/app/activity/HelpFragment.java
@@ -1,5 +1,7 @@
 package org.envirocar.app.activity;
 
+import org.envirocar.app.R;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,12 +9,18 @@ import android.view.ViewGroup;
 
 import com.actionbarsherlock.app.SherlockFragment;
 
+/**
+ * Help page
+ * 
+ * @author jakob
+ * 
+ */
 public class HelpFragment extends SherlockFragment {
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
 
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+		return inflater.inflate(R.layout.help, container, false);
+
+	}
 }
