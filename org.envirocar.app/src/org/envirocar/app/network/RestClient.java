ba5111b2cee4dbad537f0dd94946059412d496a7
matthesrieke <m.rieke@52north.org>
2013-09-03 09:45:28 +0200

    method refactoring

--- org.envirocar.app/src/org/envirocar/app/network/RestClient.java
+++ org.envirocar.app/src/org/envirocar/app/network/RestClient.java
@@ -45,28 +45,21 @@ public class RestClient {
 	}
 	
 	public static void downloadTracks(String user, String token, JsonHttpResponseHandler handler){
-		client.addHeader("X-User", user);
-		client.addHeader("X-Token", token);
-//		client.get(ECApplication.BASE_URL+"/users/"+user+"/tracks", handler);
-		client.get(ECApplication.BASE_URL+"/users/"+user+"/tracks?limit=5&page=0", handler); //TODO use pagination
+		get(ECApplication.BASE_URL+"/users/"+user+"/tracks?limit=5&page=0", handler, user, token);
 	}
 	
 	public static void deleteRemoteTrack(String user, String token, String id, JsonHttpResponseHandler handler){
-		client.addHeader("X-User", user);
-		client.addHeader("X-Token", token);
+		setHeaders(user, token);
 		client.delete(ECApplication.BASE_URL+"/users/"+user+"/tracks/" + id, handler);
 	}
 	
 	public static void downloadTrack(String user, String token, String id, JsonHttpResponseHandler handler){
-		client.addHeader("X-User", user);
-		client.addHeader("X-Token", token);
-		client.get(ECApplication.BASE_URL+"/tracks/"+id, handler);
+		get(ECApplication.BASE_URL+"/tracks/"+id, handler, user, token);
 	}
 	
 	public static boolean createSensor(String jsonObj, String user, String token, AsyncHttpResponseHandler handler){
 		client.addHeader("Content-Type", "application/json");
-		client.addHeader("X-User", user);
-		client.addHeader("X-Token", token);
+		setHeaders(user, token);
 		
 		try {
 			StringEntity se = new StringEntity(jsonObj);
@@ -78,8 +71,29 @@ public class RestClient {
 		}
 		return true;
 	}
+	
+	private static void get(String url, JsonHttpResponseHandler handler, String user, String token) {		
+		setHeaders(user, token);
+		
+		client.get(url, handler);
+	}
+
+	private static void setHeaders(String user, String token) {
+		client.addHeader("Accept-Encoding", "gzip");
+		
+		if (user != null)
+			client.addHeader("X-User", user);
+		
+		if (token != null)
+			client.addHeader("X-Token", token);
+		
+	}
 
 	public static void downloadSensors(JsonHttpResponseHandler handler){
-		client.get(ECApplication.BASE_URL+"/sensors", handler);
+		get(ECApplication.BASE_URL+"/sensors", handler);
+	}
+
+	private static void get(String url, JsonHttpResponseHandler handler) {
+		get(url, handler, null, null);
 	}
 }
