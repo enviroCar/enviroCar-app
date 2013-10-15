7c4346f7593c43501e44b914532700ea7bc352e0
matthesrieke <m.rieke@52north.org>
2013-10-15 11:30:41 +0200

    add custom views to fit the actual screen/root view size.

---
+++ org.envirocar.app/src/org/envirocar/app/views/HalfViewWidthImageView.java
@@ -0,0 +1,118 @@
+/* 
+ * enviroCar 2013
+ * Copyright (C) 2013  
+ * Martin Dueren, Jakob Moellers, Gerald Pape, Christopher Stephan
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+ * 
+ */
+package org.envirocar.app.views;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewParent;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class HalfViewWidthImageView extends ImageView {
+
+	private static final String NAMESPACE = "http://envirocar.org";
+	private static final String RELATED_VIEW_KEY = "widthRelatedPredecessingView";
+	
+	private AtomicBoolean firstRun = new AtomicBoolean(true);
+	private int relatedLayoutId = Integer.MIN_VALUE;
+
+	public HalfViewWidthImageView(Context context) {
+		super(context);
+	}
+	
+	public HalfViewWidthImageView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		
+		parseAttributes(attrs);
+	}
+	
+	public HalfViewWidthImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		
+		parseAttributes(attrs);
+	}
+
+	private void parseAttributes(AttributeSet attrs) {
+		relatedLayoutId = attrs.getAttributeResourceValue(NAMESPACE, RELATED_VIEW_KEY, Integer.MIN_VALUE);
+	}
+	
+	
+	@Override
+	protected void onDraw(Canvas canvas) {
+		applyWidth();
+		super.onDraw(canvas);
+	}
+
+	private void applyWidth() {
+		if (!firstRun.getAndSet(false)) {
+			return;
+		}
+		
+		int newWidth;
+		try {
+			newWidth = resolveRootViewWidth() / 2;
+		} catch (IllegalStateException e) {
+			return;
+		}
+		
+		int imgHeight = getDrawable().getIntrinsicHeight();
+		int imgWidth = getDrawable().getIntrinsicWidth();
+		
+		int newHeight = imgHeight * newWidth / imgWidth;
+
+		//Use RelativeLayout.LayoutParams if your parent is a RelativeLayout
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(
+		    newWidth, newHeight);
+		this.setLayoutParams(params);
+		this.setScaleType(ImageView.ScaleType.CENTER_CROP);		
+	}
+
+	private int resolveRootViewWidth() {
+		if (relatedLayoutId == Integer.MIN_VALUE) {
+			throw new IllegalStateException("Related root layout not defined.");
+		}
+		
+		int depth = 10;
+		
+		ViewParent tmp = getParent();
+		
+		View result = null;
+		while (result == null && depth-- > 0 && tmp != null) {
+			if (tmp instanceof View && ((View) tmp).getId() == relatedLayoutId) {
+				result = (View) tmp;
+			}
+			
+			tmp = tmp.getParent();
+		}
+		
+		if (result == null) {
+			throw new IllegalStateException("Related root layout not resolvable.");
+		}
+		
+		return result.getWidth();
+	}
+	
+}
