0b29ba907389c2d9e6a89296948864494bc8354a
matthesrieke <m.rieke@52north.org>
2014-02-11 17:05:58 +0100

    apply FuelingDAO specific adjustments to LogbookFragment

--- org.envirocar.app/src/org/envirocar/app/activity/LogbookFragment.java
+++ org.envirocar.app/src/org/envirocar/app/activity/LogbookFragment.java
@@ -1,29 +1,20 @@
 package org.envirocar.app.activity;
 
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
+import java.util.Date;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.message.BasicNameValuePair;
 import org.envirocar.app.R;
 import org.envirocar.app.application.CarManager;
-import org.envirocar.app.application.UserManager;
-import org.envirocar.app.model.Car.FuelType;
-import org.json.JSONObject;
+import org.envirocar.app.dao.DAOProvider;
+import org.envirocar.app.dao.exception.NotConnectedException;
+import org.envirocar.app.exception.InvalidObjectStateException;
+import org.envirocar.app.json.FuelingEncoder;
+import org.envirocar.app.model.Fueling;
+import org.envirocar.app.model.NumberWithUOM;
 
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
 import android.content.DialogInterface.OnClickListener;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.util.Log;
@@ -45,9 +36,9 @@ import com.actionbarsherlock.app.SherlockFragment;
 		private Button btn;
 		
 		//Date and Time
-		Calendar c = Calendar.getInstance();
-		SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm");
-		String formattedDate = df.format(c.getTime());
+//		Calendar c = Calendar.getInstance();
+//		SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm");
+//		String formattedDate = df.format(c.getTime());
 		
 		
 		public View onCreateView(android.view.LayoutInflater inflater,
@@ -100,36 +91,56 @@ import com.actionbarsherlock.app.SherlockFragment;
 				settings.edit().putString(Default_Volume_Unit, Unit1.getText().toString()).commit();
 				settings.edit().putString(Default_Currency_Unit, Currency.getText().toString()).commit();
 				
-				String Username = UserManager.instance().getUser().getUsername();
-				String carmodel = CarManager.instance().getCar().getModel();
-				FuelType fueltype = CarManager.instance().getCar().getFuelType();
+//				String Username = UserManager.instance().getUser().getUsername();
+//				String carmodel = CarManager.instance().getCar().getModel();
+//				FuelType fueltype = CarManager.instance().getCar().getFuelType();
 				
-				JSONObject obj1=new JSONObject();
-				obj1.put("DateTime:", formattedDate);
-				obj1.put("Note:", Note.getText());
-				JSONObject obj2=new JSONObject();
-				obj2.put("Name:", Username );
-				obj2.put("Car:", carmodel);
-				obj2.put("Fueltype:",fueltype);
-				obj1.put("User:", obj2);
-				JSONObject obj3=new JSONObject();
-				obj3.put("Volume:", Volume.getText());
-				obj3.put("VolumeUnit:", Unit1.getText());
-				obj1.put("Fuel Tank:", obj3);
-				JSONObject obj4=new JSONObject();
-				obj4.put("Cost:", Cost.getText());
-				obj4.put("Currency:", Currency.getText());
-				obj1.put("Finance:", obj4);
-				JSONObject obj5=new JSONObject();
-				obj5.put("Distance:", Distance.getText());
-				obj5.put("DistanceUnit:", Unit2.getText());
-				obj1.put("Total traveled Distance:", obj5);
-							
-				String str=obj1.toString();
+				Fueling fueling = new Fueling();
+				fueling.setTime(new Date());
+				fueling.setCar(CarManager.instance().getCar());
+				if (!Note.getText().toString().isEmpty()) {
+					fueling.setComment(Note.getText().toString());
+				}
+				
+				try {
+					fueling.setCost(new NumberWithUOM(Double.parseDouble(Cost.getText().toString()),
+							Currency.getText().toString()));
+					fueling.setVolume(new NumberWithUOM(Double.parseDouble(Volume.getText().toString()),
+							Unit1.getText().toString()));
+					fueling.setMileage(new NumberWithUOM(Double.parseDouble(Distance.getText().toString()),
+							Unit2.getText().toString()));	
+				}
+				catch (NumberFormatException e) {
+					//TODO implement some valuable user feedback
+					throw e;
+				}
+				
+//				JSONObject obj1=new JSONObject();
+//				obj1.put("DateTime:", formattedDate);
+//				obj1.put("Note:", Note.getText());
+//				JSONObject obj2=new JSONObject();
+//				obj2.put("Name:", Username );
+//				obj2.put("Car:", carmodel);
+//				obj2.put("Fueltype:",fueltype);
+//				obj1.put("User:", obj2);
+//				JSONObject obj3=new JSONObject();
+//				obj3.put("Volume:", Volume.getText());
+//				obj3.put("VolumeUnit:", Unit1.getText());
+//				obj1.put("Fuel Tank:", obj3);
+//				JSONObject obj4=new JSONObject();
+//				obj4.put("Cost:", Cost.getText());
+//				obj4.put("Currency:", Currency.getText());
+//				obj1.put("Finance:", obj4);
+//				JSONObject obj5=new JSONObject();
+//				obj5.put("Distance:", Distance.getText());
+//				obj5.put("DistanceUnit:", Unit2.getText());
+//				obj1.put("Total traveled Distance:", obj5);
+//							
+//				String str=obj1.toString();
 				//Sending the JSON-Object
-				new MyAsyncTask().execute(str);
-					//Log-Entry to LogCat (for Testing)
-					Log.i("JsonString :", str);
+				new MyAsyncTask().execute(fueling);
+				//Log-Entry to LogCat (for Testing)
+				Log.i("JsonString :", new FuelingEncoder().createFuelingJson(fueling).toString());
 				 
 			}
 		catch (Exception je)
@@ -139,10 +150,10 @@ import com.actionbarsherlock.app.SherlockFragment;
 			}
 		}
 			
-		private class MyAsyncTask extends AsyncTask<String, Integer, Double>{
+		private class MyAsyncTask extends AsyncTask<Fueling, Integer, Double>{
 			 
 			@Override
-			protected Double doInBackground(String... params) {
+			protected Double doInBackground(Fueling... params) {
 			// TODO Auto-generated method stub
 			postData(params[0]);
 			return null;
@@ -153,26 +164,35 @@ import com.actionbarsherlock.app.SherlockFragment;
 			}
 			
 			 
-			public void postData(String valueIWantToSend) {
+			public void postData(Fueling valueIWantToSend) {
 			// Create a new HttpClient and Post Header. Also HTTP-Adress from Server
-			HttpClient httpclient = new DefaultHttpClient();
-			HttpPost httppost = new HttpPost("http://ows.dev.52north.org/enviroCar/receiver.php");
-			 
-			try {
-			// Where to add the JSON-Object (watch reciever.php also)
-			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
-			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-			 
-			// Execute HTTP Post Request
-			HttpResponse response = httpclient.execute(httppost);
-			 
-			} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			} catch (IOException e) {
-			// TODO Auto-generated catch block
+//			HttpClient httpclient = new DefaultHttpClient();
+//			HttpPost httppost = new HttpPost("http://ows.dev.52north.org/enviroCar/receiver.php");
+//			 
+//			try {
+//			// Where to add the JSON-Object (watch reciever.php also)
+//			List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+//			nameValuePairs.add(new BasicNameValuePair("myHttpData", valueIWantToSend));
+//			httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+//			 
+//			// Execute HTTP Post Request
+//			HttpResponse response = httpclient.execute(httppost);
+//			 
+//			} catch (ClientProtocolException e) {
+//			// TODO Auto-generated catch block
+//			} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			}
+				try {
+					DAOProvider.instance().getFuelingDAO().storeFueling(valueIWantToSend);
+				} catch (NotConnectedException e) {
+					Toast.makeText(LogbookFragment.this.getActivity(), "Please check if you're logged in,have chosen a car or filled in all input fields.", Toast.LENGTH_LONG).show();
+				} catch (InvalidObjectStateException e) {
+					Toast.makeText(LogbookFragment.this.getActivity(), "Please check if you're logged in,have chosen a car or filled in all input fields.", Toast.LENGTH_LONG).show();
+				}
 			}
-			}
+				
+			
 			 
 			}
 	 
