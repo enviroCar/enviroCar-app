e410f8884b40f6b3d166a955e0aedf0d9221eb3e
dewall <a.dewall@uni-muenster.de>
2015-10-09 12:47:44 +0200

    created obd module (left files)

--- org.envirocar.obd/src/main/java/org/envirocar/obd/service/BluetoothBackgroundService.java
+++ org.envirocar.obd/src/main/java/org/envirocar/obd/service/BluetoothBackgroundService.java
@@ -1,15 +1,15 @@
-package org.envirocar.obd.service;
-
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
-
-/**
- * @author dewall
- */
-public class BluetoothBackgroundService extends Service {
-    @Override
-    public IBinder onBind(Intent intent) {
-        return null;
-    }
-}
+//package org.envirocar.obd.service;
+//
+//import android.app.Service;
+//import android.content.Intent;
+//import android.os.IBinder;
+//
+///**
+// * @author dewall
+// */
+//public class BluetoothBackgroundService extends Service {
+//    @Override
+//    public IBinder onBind(Intent intent) {
+//        return null;
+//    }
+//}
