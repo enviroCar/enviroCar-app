e03aebd4e8b29a3fc0d6fdd220cafca536f7f845
matthesrieke <m.rieke@52north.org>
2015-10-27 16:43:14 +0100

    fix capital errors...

--- org.envirocar.obd/src/main/java/org/envirocar/obd/protocol/OBDCommandLooper.java
+++ org.envirocar.obd/src/main/java/org/envirocar/obd/protocol/OBDCommandLooper.java
@@ -41,6 +41,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
