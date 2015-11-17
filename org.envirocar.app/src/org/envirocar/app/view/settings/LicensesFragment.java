37d93b3d586f7963d3914634774e349af96d0fce
dewall <a.dewall@52north.org>
2015-11-17 11:37:56 +0100

    added licenses dialog in settings

--- org.envirocar.app/src/org/envirocar/app/view/settings/LicensesFragment.java
+++ org.envirocar.app/src/org/envirocar/app/view/settings/LicensesFragment.java
@@ -1,7 +1,44 @@
 package org.envirocar.app.view.settings;
 
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import org.envirocar.app.R;
+
 /**
- * Created by adewa on 11/16/2015.
+ * TODO JavaDoc
+ *
+ * @author dewall
  */
-public class LicensesFragment {
+public class LicensesFragment extends DialogFragment {
+
+    public static LicensesFragment newInstance(){
+        return new LicensesFragment();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if(dialog != null){
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            dialog.getWindow().setLayout(width, height);
+//            dialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+//                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle
+            savedInstanceState) {
+        View view = inflater.inflate(R.layout.preferences_fragment_licenses, container, false);
+        return view;
+    }
 }
--- org.envirocar.app/src/org/envirocar/app/view/settings/OtherSettingsFragment.java
+++ org.envirocar.app/src/org/envirocar/app/view/settings/OtherSettingsFragment.java
@@ -1,11 +1,14 @@
 package org.envirocar.app.view.settings;
 
+import android.app.AlertDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.webkit.WebView;
 
 import org.envirocar.app.R;
 import org.envirocar.core.util.Util;
@@ -16,6 +19,7 @@ import org.envirocar.core.util.Util;
 public class OtherSettingsFragment extends PreferenceFragment {
     private static final String KEY_PREF_WEBSITE = "preference_other_website";
     private static final String KEY_PREF_VERSION = "preference_other_version";
+    private static final String KEY_PREF_LICENSES = "preferences_other_licenses";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +40,15 @@ public class OtherSettingsFragment extends PreferenceFragment {
         // Set the version of the application as
         Preference version = findPreference(KEY_PREF_VERSION);
         version.setSummary(Util.getVersionString(getActivity()));
+
+        Preference licenses = findPreference(KEY_PREF_LICENSES);
+        licenses.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+            @Override
+            public boolean onPreferenceClick(Preference preference) {
+                displayLicenseDialog();
+                return true;
+            }
+        });
     }
 
     @Override
@@ -44,5 +57,16 @@ public class OtherSettingsFragment extends PreferenceFragment {
 
         // Set the background color from transparent to white.
         getView().setBackgroundColor(getResources().getColor(R.color.white_cario));
+    }
+
+    private void displayLicenseDialog() {
+        WebView webView = (WebView) LayoutInflater.from(getActivity()).inflate(R.layout
+                .preference_fragment_licenses, null);
+        webView.loadUrl("file:///android_asset/open_source_licenses.html");
+        new AlertDialog.Builder(getActivity())
+                .setTitle("Open Source Licenses\nNotices for libraries:")
+                .setView(webView)
+                .setPositiveButton("Ok", null)
+                .show();
     }
 }
