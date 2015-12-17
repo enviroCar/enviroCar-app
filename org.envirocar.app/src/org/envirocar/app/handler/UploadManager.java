087eaa5cc7b43123e0ce6cbc48f48aa11130a1e9
matthesrieke <m.rieke@52north.org>
2015-12-17 12:16:03 +0100

    cascading throw of NoMeasurementsException on obfuscation

--- org.envirocar.app/src/org/envirocar/app/handler/UploadManager.java
+++ org.envirocar.app/src/org/envirocar/app/handler/UploadManager.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (C) 2013 - 2015 the enviroCar community
- * <p>
+ *
  * This file is part of the enviroCar app.
- * <p>
+ *
  * The enviroCar app is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * The enviroCar app is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License along
  * with the enviroCar app. If not, see http://www.gnu.org/licenses/.
  */
@@ -234,14 +234,17 @@ public class UploadManager {
                     if (callback != null) {
                         callback.onSuccessfulUpload(track);
                     }
-                } catch (Exception e) {
-                    if (track.getMeasurements().size() != 0) {
+                } catch (ResourceConflictException | NotConnectedException | DataCreationFailureException
+                        | UnauthorizedException | NoMeasurementsException e) {
+                    logger.error(e.getMessage(), e);
+                    if (track.getMeasurements().size() == 0) {
                         alertOnObfuscationMeasurements();
                     }
-                    logger.error(e.getMessage(), e);
-                    mNotificationHandler.createNotification(mContext
-                            .getString(R.string
-                                    .general_error_please_report));
+                    else {
+                        mNotificationHandler.createNotification(mContext
+                                .getString(R.string
+                                        .general_error_please_report));
+                    }
                 }
 
                 return null;
