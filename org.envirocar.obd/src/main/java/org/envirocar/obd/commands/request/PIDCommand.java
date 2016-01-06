1230f61a69d4f49ad505b9c20f98515369107295
matthesrieke <m.rieke@52north.org>
2016-01-06 17:04:52 +0100

    add some javadoc for PID reponse line stuff

--- org.envirocar.obd/src/main/java/org/envirocar/obd/commands/request/PIDCommand.java
+++ org.envirocar.obd/src/main/java/org/envirocar/obd/commands/request/PIDCommand.java
@@ -9,12 +9,27 @@ public class PIDCommand implements BasicCommand {
     private PID pid;
     private byte[] bytes;
 
+    /**
+     * @param mode the mode of the PID cmd
+     * @param pid the PID to be requested
+     *
+     */
     public PIDCommand(String mode, PID pid) {
         this(mode, pid, 1);
     }
 
+    /**
+     * @param mode the mode of the PID cmd
+     * @param pid the PID to be requested
+     * @param expectedResponseLines the number of response lines to be expected after execution.
+     *                              The OBD adapter will wait for this amount of lines and write
+     *                              the response immediately after reception of it. E.g. providing 1
+     *                              results in a fast response. Providing 0 will result in the
+     *                              default OBD behaviour of waiting 200ms for responses and return
+     *                              the first.
+     */
     public PIDCommand(String mode, PID pid, int expectedResponseLines) {
-        if (expectedResponseLines < 1 || expectedResponseLines > 9) {
+        if (expectedResponseLines < 0 || expectedResponseLines > 9) {
             throw new IllegalStateException("expectedResponseLines out of allowed bounds");
         }
 
@@ -29,7 +44,7 @@ public class PIDCommand implements BasicCommand {
         int ml = mode.length();
         String pidString = pid.getHexadecimalRepresentation();
         int pl = pidString.length();
-        bytes = new byte[ml + pl + 1 + 1];
+        bytes = new byte[ml + pl + 1 + (expectedResponseLines == 0 ? 0 : 1)];
 
         int pos = 0;
         for (int i = 0; i < ml; i++) {
@@ -42,7 +57,9 @@ public class PIDCommand implements BasicCommand {
             bytes[pos++] = (byte) pidString.charAt(i);
         }
 
-        bytes[pos++] = (byte) Integer.toString(this.expectedResponseLines).charAt(0);
+        if (expectedResponseLines > 0) {
+            bytes[pos++] = (byte) Integer.toString(this.expectedResponseLines).charAt(0);
+        }
     }
 
     public String getMode() {
