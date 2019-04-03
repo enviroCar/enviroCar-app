778c62839bd068b7580a1eae72f4f72fce65972b
chenineazeddine <chenineazeddine5@gmail.com>
2019-04-03 15:46:12 +0100

    updating the local track cardview

--- org.envirocar.app/src/org/envirocar/app/view/tracklist/TrackListRemoteCardAdapter.java
+++ org.envirocar.app/src/org/envirocar/app/view/tracklist/TrackListRemoteCardAdapter.java
@@ -19,6 +19,7 @@
 package org.envirocar.app.view.tracklist;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
