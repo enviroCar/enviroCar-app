a7df897fd0bad6a7327078f36588c243e66435ec
Shiva Kanth <shivakanth.sujit@gmail.com>
2019-05-29 14:39:41 +0530

    Fix: used the wrong version for the RxJava adapter for Retrofit

--- org.envirocar.remote/src/main/java/org/envirocar/remote/RemoteModule.java
+++ org.envirocar.remote/src/main/java/org/envirocar/remote/RemoteModule.java
@@ -67,7 +67,7 @@ import dagger.Provides;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
@@ -233,7 +233,7 @@ public class RemoteModule {
                 .client(client)
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory.create(gson))
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                 .build();
     }
 
