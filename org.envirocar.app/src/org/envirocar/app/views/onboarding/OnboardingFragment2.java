e3f568af0aca672f65cf5284083d83cd7a2a4d3b
Shiva Kanth Sujit <shivakanth.sujit@gmail.com>
2019-08-03 20:01:19 +0530

    OBD Onboarding implemented

--- org.envirocar.app/src/org/envirocar/app/views/onboarding/OnboardingFragment2.java
+++ org.envirocar.app/src/org/envirocar/app/views/onboarding/OnboardingFragment2.java
@@ -1,14 +1,20 @@
 package org.envirocar.app.views.onboarding;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 import org.envirocar.app.R;
+import org.w3c.dom.Text;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,11 +26,22 @@ public class OnboardingFragment2 extends Fragment {
         // Required empty public constructor
     }
 
+    @BindView(R.id.obd_onbaording_start)
+    protected TextView obdOBStart;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.onboarding_2,container,false);
+        View view = inflater.inflate(R.layout.onboarding_2,container,false);
+        ButterKnife.bind(this, view);
+        obdOBStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(),OnboardingOBDActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
 }
---
+++ org.envirocar.app/src/org/envirocar/app/views/onboarding/OnboardingOBDActivity.java
@@ -0,0 +1,146 @@
+package org.envirocar.app.views.onboarding;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimatedVectorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.envirocar.app.R;
+import org.envirocar.core.logging.Logger;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class OnboardingOBDActivity extends Activity {
+    private static final Logger LOG = Logger.getLogger(OnboardingOBDActivity.class);
+
+    @BindView(R.id.donePage1)
+    protected TextView doneP1;
+    @BindView(R.id.donePage2)
+    protected TextView doneP2;
+    @BindView(R.id.skipPage1)
+    protected TextView skipP1;
+    @BindView(R.id.skipPage2)
+    protected TextView skipP2;
+    @BindView(R.id.obdTroubleshoot)
+    protected TextView obdTroubleshoot;
+    @BindView(R.id.obdLink)
+    protected TextView obdLink;
+    @BindView(R.id.page1)
+    protected ConstraintLayout page1;
+    @BindView(R.id.page2)
+    protected ConstraintLayout page2;
+    @BindView(R.id.page3)
+    protected ConstraintLayout page3;
+    @BindView(R.id.continueButton)
+    protected Button continueButton;
+    @BindView(R.id.animatedView)
+    protected ImageView animatedcheck;
+
+    protected int whichPage = 0;
+    AnimatedVectorDrawable frameAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.onboarding_obd);
+        ButterKnife.bind(this);
+        animatedcheck.setBackgroundResource(R.drawable.animated_check);
+        frameAnimation = (AnimatedVectorDrawable) animatedcheck.getBackground();
+
+        obdLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.amazon.de/Cartrend-80290-Bluetooth-Controller-Smartphones/dp/B00CCZJPCU/ref=sr_1_2?ie=UTF8&qid=1513252484&sr=8-2&keywords=cartrend+obd2");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+                }
+        });
+
+        obdTroubleshoot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("https://www.hum.com/port/");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        skipP1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        skipP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        continueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+        doneP1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(whichPage == 0){
+                    whichPage = 1;
+                    page1.setVisibility(View.GONE);
+                    page2.setVisibility(View.VISIBLE);
+                    page3.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        doneP2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(whichPage == 1){
+                    whichPage = 2;
+                    page1.setVisibility(View.GONE);
+                    page2.setVisibility(View.GONE);
+                    page3.setVisibility(View.VISIBLE);
+                    frameAnimation.start();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        LOG.info("whichPage: " + whichPage);
+        if(frameAnimation.isRunning()){
+            frameAnimation.stop();
+        }
+        if(whichPage == 0){
+            super.onBackPressed();
+        }
+        else if(whichPage == 1){
+            whichPage =0;
+            page1.setVisibility(View.VISIBLE);
+            page2.setVisibility(View.GONE);
+            page3.setVisibility(View.GONE);
+        }else if(whichPage == 2){
+            whichPage =1;
+            page1.setVisibility(View.GONE);
+            page2.setVisibility(View.VISIBLE);
+            page3.setVisibility(View.GONE);
+        }
+    }
+}
