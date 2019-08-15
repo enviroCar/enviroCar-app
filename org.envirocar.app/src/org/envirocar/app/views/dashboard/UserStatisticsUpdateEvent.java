fc9a2065cf7bb668a4cbdec0b986068b0d3fe846
Wall, Arne de (52-North) <a.dewall@52north.org>
2019-08-15 16:05:37 +0200

    implemented statistics processor for user related track statistics

---
+++ org.envirocar.app/src/org/envirocar/app/views/dashboard/UserStatisticsUpdateEvent.java
@@ -0,0 +1,35 @@
+package org.envirocar.app.views.dashboard;
+
+import com.google.common.base.MoreObjects;
+
+/**
+ * @author dewall
+ */
+public class UserStatisticsUpdateEvent {
+
+    public final int numTracks;
+    public final double totalDistance;
+    public final long totalDuration;
+
+    /**
+     * Constructor.
+     *
+     * @param numTracks
+     * @param totalDistance
+     * @param totalDuration
+     */
+    public UserStatisticsUpdateEvent(int numTracks, double totalDistance, long totalDuration) {
+        this.numTracks = numTracks;
+        this.totalDistance = totalDistance;
+        this.totalDuration = totalDuration;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("Number of Tracks", numTracks)
+                .add("Total Distance", totalDistance)
+                .add("Total Duration", totalDuration)
+                .toString();
+    }
+}
