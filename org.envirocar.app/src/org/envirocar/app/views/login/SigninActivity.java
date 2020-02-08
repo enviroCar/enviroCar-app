68dfabf0238257bf81423201601888d8b18ea97f
ARVIND NEGI <negi.arvind03@gmail.com>
2020-02-08 11:56:00 +0530

    used butter knife for Editor action

--- org.envirocar.app/src/org/envirocar/app/views/login/SigninActivity.java
+++ org.envirocar.app/src/org/envirocar/app/views/login/SigninActivity.java
@@ -21,14 +21,10 @@ package org.envirocar.app.views.login;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.core.app.ActivityOptionsCompat;
 
@@ -49,6 +45,7 @@ import javax.inject.Inject;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnEditorAction;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.observers.DisposableCompletableObserver;
@@ -59,7 +56,7 @@ import io.reactivex.schedulers.Schedulers;
  *
  * @author dewall
  */
-public class SigninActivity extends BaseInjectorActivity implements TextView.OnEditorActionListener {
+public class SigninActivity extends BaseInjectorActivity {
     private static final Logger LOG = Logger.getLogger(SigninActivity.class);
 
     public static void startActivity(Context context){
@@ -99,8 +96,6 @@ public class SigninActivity extends BaseInjectorActivity implements TextView.OnE
 
         // inject the views
         ButterKnife.bind(this);
-
-        passwordEditText.setOnEditorActionListener(this);
     }
 
     @Override
@@ -121,6 +116,15 @@ public class SigninActivity extends BaseInjectorActivity implements TextView.OnE
         SignupActivity.startActivity(this);
     }
 
+    @OnEditorAction(R.id.activity_login_password_input)
+    protected boolean implicitSubmit()  {
+
+        View logInSubmit ;
+        logInSubmit=    findViewById(R.id.activity_signin_login_button);
+        logInSubmit.performClick();
+        return true;
+
+    }
     @OnClick(R.id.activity_signin_login_button)
     protected void onLoginClicked() {
         LOG.info("Clicked on the login button");
@@ -162,6 +166,7 @@ public class SigninActivity extends BaseInjectorActivity implements TextView.OnE
             this.login(username, password);
         }
     }
+
     private void login(String username, String password) {
         // Create a dialog indicating the log in process.
         this.loginSubscription = userHandler.logIn(username, password)
@@ -211,17 +216,5 @@ public class SigninActivity extends BaseInjectorActivity implements TextView.OnE
                         }
                     }
                 });
-    }
-
-    @Override
-    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-        if(i== EditorInfo.IME_ACTION_DONE)  {
-
-            View signInButton ;
-            signInButton =  findViewById(R.id.activity_signin_login_button);
-            signInButton.performClick();
-            return true;
-        }
-        return false;
     }
 }
