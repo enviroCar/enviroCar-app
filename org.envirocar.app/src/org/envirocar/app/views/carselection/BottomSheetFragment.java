7c61b776a77c5f564837e8b9f102e6d30575e7f8
ARVIND NEGI <negi.arvind03@gmail.com>
2020-06-27 12:07:14 +0530

    added modal bottom sheet layout

---
+++ org.envirocar.app/src/org/envirocar/app/views/carselection/BottomSheetFragment.java
@@ -0,0 +1,29 @@
+package org.envirocar.app.views.carselection;
+
+import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+
+import org.envirocar.app.R;
+
+public class BottomSheetFragment extends BottomSheetDialogFragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.car_attributes_detail_bottomsheet,container);
+        return view;
+    }
+
+
+}
