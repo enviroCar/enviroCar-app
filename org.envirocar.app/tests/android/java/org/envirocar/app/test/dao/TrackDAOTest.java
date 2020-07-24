0318694736cfda8061a605a4aafd6bbd202770b6
ARVIND NEGI <negi.arvind03@gmail.com>
2020-07-24 22:32:26 +0530

    added new car for test

--- org.envirocar.app/tests/android/java/org/envirocar/app/test/dao/TrackDAOTest.java
+++ org.envirocar.app/tests/android/java/org/envirocar/app/test/dao/TrackDAOTest.java
@@ -6,10 +6,13 @@ import androidx.test.InstrumentationRegistry;
 import androidx.test.runner.AndroidJUnit4;
 
 import org.envirocar.core.EnviroCarDB;
+import org.envirocar.core.entity.Car;
+import org.envirocar.core.entity.CarImpl;
 import org.envirocar.storage.EnviroCarDBImpl;
 import org.envirocar.storage.TrackRoomDatabase;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
@@ -17,6 +20,8 @@ public class TrackDAOTest {
 
     private EnviroCarDB enviroCarDB;
     private TrackRoomDatabase trackRoomDatabase;
+
+   // Room database instance in memory for test
     @Before
     public void initTrackDb() throws Exception {
          trackRoomDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
@@ -29,5 +34,24 @@ public class TrackDAOTest {
     @After
     public  void closeTrackDb() throws Exception {
         trackRoomDatabase.close();
+    }
+
+    @Test
+    public void trackTest() {
+
+
+    }
+
+    // create new car
+    private Car createCar() {
+        Car car = new CarImpl();
+        car.setId("5750591ee4b09078f98673d8");
+        car.setConstructionYear(2004);
+        car.setManufacturer("Opel");
+        car.setModel("Vectra C Caravan");
+        car.setFuelType(Car.FuelType.GASOLINE);
+        car.setEngineDisplacement(2200);
+
+        return car;
     }
 }
