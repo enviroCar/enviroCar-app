84d6da24523f512ced34ca0bc1fff5640f96135e
ARVIND NEGI <negi.arvind03@gmail.com>
2020-07-28 01:56:12 +0530

    added fragment for obd help content

---
+++ org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdContent2Fragment.java
@@ -0,0 +1,23 @@
+package org.envirocar.app.views.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import org.envirocar.app.R;
+
+public class ObdContent2Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View contentView = inflater.inflate(R.layout.obd_dialog_content2,container,false);
+        return contentView;
+    }
+}
