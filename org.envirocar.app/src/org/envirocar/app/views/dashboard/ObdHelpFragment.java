b156d08c2a2dc96f068911b78911716925399c70
ARVIND NEGI <negi.arvind03@gmail.com>
2020-07-28 12:05:39 +0530

    bind view using butter knife and added on click functionality to button

--- org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdHelpFragment.java
+++ org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdHelpFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -14,20 +15,64 @@ import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import org.envirocar.app.R;
-import org.envirocar.app.views.others.OthersFragment;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 
 public class ObdHelpFragment extends DialogFragment {
+    @BindView(R.id.obd_help_viewpager)
+    ViewPager viewPager;
+    @BindView(R.id.obd_help_cancel)
+    TextView helpCancel;
+    @BindView(R.id.obd_help_next)
+    TextView helpNext;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View contentView = inflater.inflate(R.layout.obd_help_dialog,container,false);
-        ViewPager viewPager = contentView.findViewById(R.id.obd_help_viewpager);
+        View contentView = inflater.inflate(R.layout.obd_help_dialog, container, false);
+
+        ButterKnife.bind(this, contentView);
 
         FragmentStatePagerAdapter fragmentStatePagerAdapter;
         fragmentStatePagerAdapter = new OBDPager(getChildFragmentManager());
         viewPager.setAdapter(fragmentStatePagerAdapter);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (position == 2) {
+                    helpNext.setVisibility(View.GONE);
+                } else {
+                    helpNext.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
         return contentView;
+    }
+
+    @OnClick(R.id.obd_help_next)
+    void nextClick() {
+        viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+    }
+
+    @OnClick(R.id.obd_help_cancel)
+    void cancelClick() {
+        dismiss();
     }
 
     private class OBDPager extends FragmentStatePagerAdapter {
@@ -36,10 +81,11 @@ public class ObdHelpFragment extends DialogFragment {
         ObdContent1Fragment obdContent1Fragment = new ObdContent1Fragment();
         ObdContent2Fragment obdContent2Fragment = new ObdContent2Fragment();
         ObdContent3Fragment obdContent3Fragment = new ObdContent3Fragment();
+
         public OBDPager(@NonNull FragmentManager fm) {
             super(fm);
-            fragments = new Fragment[] {
-                   obdContent1Fragment,
+            fragments = new Fragment[]{
+                    obdContent1Fragment,
                     obdContent2Fragment,
                     obdContent3Fragment
             };
