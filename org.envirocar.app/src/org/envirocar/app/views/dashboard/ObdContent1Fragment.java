329ad9189daf63ec072b736ee7a1dcaafabc71a9
ARVIND NEGI <negi.arvind03@gmail.com>
2020-07-29 22:36:38 +0530

    redesign obd help content fragment

--- org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdContent1Fragment.java
+++ org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdContent1Fragment.java
@@ -1,9 +1,12 @@
 package org.envirocar.app.views.dashboard;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +14,28 @@ import androidx.fragment.app.Fragment;
 
 import org.envirocar.app.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ObdContent1Fragment extends Fragment {
+
+    @BindView(R.id.obdHelpHead)
+    TextView obdHelpHead;
+    @BindView(R.id.obdHelpIamge)
+    ImageView imageView;
+    @BindView(R.id.obdHelpText)
+    TextView obdHelpText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View contentView = inflater.inflate(R.layout.obd_dialog_content1, container, false);
+        View contentView = inflater.inflate(R.layout.obd_help_layout, container, false);
+        ButterKnife.bind(this, contentView);
+
+        Drawable image = getActivity().getDrawable(R.drawable.findobddesgin);
+        imageView.setImageDrawable(image);
+        obdHelpText.setText(getString(R.string.obd_help_content));
         return contentView;
     }
 }
--- org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdContent2Fragment.java
+++ org.envirocar.app/src/org/envirocar/app/views/dashboard/ObdContent2Fragment.java
@@ -1,9 +1,12 @@
 package org.envirocar.app.views.dashboard;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +14,29 @@ import androidx.fragment.app.Fragment;
 
 import org.envirocar.app.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class ObdContent2Fragment extends Fragment {
+
+    @BindView(R.id.obdHelpHead)
+    TextView obdHelpHead;
+    @BindView(R.id.obdHelpIamge)
+    ImageView imageView;
+    @BindView(R.id.obdHelpText)
+    TextView obdHelpText;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        View contentView = inflater.inflate(R.layout.obd_dialog_content2, container, false);
+        View contentView = inflater.inflate(R.layout.obd_help_layout, container, false);
+        ButterKnife.bind(this, contentView);
+
+        Drawable image = getActivity().getDrawable(R.drawable.plugdesignfinal);
+        imageView.setImageDrawable(image);
+        obdHelpHead.setText("Plug");
+        obdHelpText.setText(getText(R.string.obd_help_content2));
         return contentView;
     }
 }
