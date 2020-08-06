565513c4d5d6ffc6bb8ebc3f6b1be0a5ff1f792c
ARVIND NEGI <negi.arvind03@gmail.com>
2020-08-06 17:51:07 +0530

    modified query return type and added query to fetch based on manufacture id and name

--- org.envirocar.storage/src/main/java/org/envirocar/storage/dao/LocalVehicleDAO.java
+++ org.envirocar.storage/src/main/java/org/envirocar/storage/dao/LocalVehicleDAO.java
@@ -7,13 +7,17 @@ import org.envirocar.core.entity.Vehicles;
 
 import java.util.List;
 
+import io.reactivex.Observable;
 import io.reactivex.Single;
 
 @Dao
 public interface LocalVehicleDAO extends BaseLocalCarDAO<Vehicles> {
 
-    @Query("SELECT * FROM vehicles")
-    Single<List<Vehicles>> getManufacturerVehicles();
+    @Query("SELECT * FROM vehicles WHERE manufacturer_id = :manuId")
+    Observable<List<Vehicles>> getManufacturerVehiclesId(String manuId);
+
+    @Query("SELECT * FROM vehicles WHERE manufacturer = :manuName")
+    Observable<List<Vehicles>> getManufacturerVehicles(String manuName);
 
     @Query("SELECT * FROM vehicles where manufacturer_id = :manuId AND id = :vehicleId")
     Single<Vehicles> getHsnTsnVehicle(String manuId, String vehicleId);
