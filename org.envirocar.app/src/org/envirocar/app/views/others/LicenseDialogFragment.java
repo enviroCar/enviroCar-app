0da843f1c4b0b199bb59a2f6d72e4b026c7e82c7
ARVIND NEGI <negi.arvind03@gmail.com>
2020-08-08 22:20:52 +0530

    added license dialog fragment

--- org.envirocar.app/src/org/envirocar/app/views/others/LicenseDialogFragment.java
+++ org.envirocar.app/src/org/envirocar/app/views/others/LicenseDialogFragment.java
@@ -1,4 +1,44 @@
 package org.envirocar.app.views.others;
 
-public class LicenseDialogFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+import org.envirocar.app.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LicenseDialogFragment extends DialogFragment {
+
+    @BindView(R.id.licenseWebView)
+    WebView webView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_license, container, false);
+
+        ButterKnife.bind(this, view);
+        webView.loadUrl("file:///android_asset/open_source_licenses.html");
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        getDialog().getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @OnClick(R.id.licenseDialogClose)
+    void onClickClose() {
+        dismiss();
+    }
 }
