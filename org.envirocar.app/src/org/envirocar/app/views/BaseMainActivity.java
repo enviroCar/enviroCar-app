49e466d7cf54fe690df5cd4b79bf6131ab0b2d83
Amey Thakur <amey36thakur@gmail.com>
2021-03-18 21:14:40 +0530

    Shows warning after pressing back button

--- org.envirocar.app/src/org/envirocar/app/views/BaseMainActivity.java
+++ org.envirocar.app/src/org/envirocar/app/views/BaseMainActivity.java
@@ -23,8 +23,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -262,6 +264,27 @@ public class BaseMainActivity extends BaseInjectorActivity {
         }
     }
 
+    boolean doubleBackToExitPressedOnce = false;
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce=false;
+            }
+        }, 2000);
+    }
+
     private void addPreferenceSubscriptions() {
         // Keep screen active setting;
         subscriptions.add(
@@ -352,5 +375,9 @@ public class BaseMainActivity extends BaseInjectorActivity {
         public int getCount() {
             return 3;
         }
+
+        boolean doubleBackToExitPressedOnce = false;
+
+
     }
 }
