75bff4817dc2a9796dfff99f334a757fe7b930f9
asaikarthikeya <a.saikarthikeya@gmail.com>
2021-06-16 10:51:14 +0530

    replace icon in Log delete

--- org.envirocar.app/src/org/envirocar/app/views/logbook/LogbookActivity.java
+++ org.envirocar.app/src/org/envirocar/app/views/logbook/LogbookActivity.java
@@ -145,7 +145,7 @@ public class LogbookActivity extends BaseInjectorActivity implements LogbookUiLi
                         .genral_dialog_toolbar);
                 toolbar1.setTitle(R.string.logbook_dialog_delete_fueling_header);
                 toolbar1.setNavigationIcon(ContextCompat.getDrawable(LogbookActivity.this,
-                        R.drawable.others_logbook_white_24dp));
+                        R.drawable.ic_delete_fueling_24));
                 toolbar1.setTitleTextColor(
                         getResources().getColor(R.color.white_cario));
 
