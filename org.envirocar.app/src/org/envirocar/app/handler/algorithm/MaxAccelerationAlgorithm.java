7a70501cbb040a4ab931819f58cef8b5e6ecf9d6
Sebastian Drost <s.drost@52north.org>
2021-11-05 14:59:59 +0100

    Implement min and max acceleration algorithm

--- org.envirocar.app/src/org/envirocar/app/handler/algorithm/MaxAccelerationAlgorithm.java
+++ org.envirocar.app/src/org/envirocar/app/handler/algorithm/MaxAccelerationAlgorithm.java
@@ -5,11 +5,25 @@ import org.envirocar.obd.events.PropertyKeyEvent;
 
 import java.util.List;
 
-public class MaxAccelerationAlgorithm implements DataResponseAlgorithm {
+public class MaxAccelerationAlgorithm extends AbstractAccelerationAlgorithm implements DataResponseAlgorithm {
 
     @Override
-    public double calculate(List<PropertyKeyEvent> pke) {
-        return Math.random() * 5;
+    public Double calculate(List<PropertyKeyEvent> pkes) {
+        if(pkes.size() < 2) {
+            return null;
+        }
+        double maxAcc = calculateAcceleration(pkes.get(0).getValue(), pkes.get(1).getValue(),
+                pkes.get(0).getTimestamp(), pkes.get(1).getTimestamp());
+
+        for(int i = 1; i < pkes.size() - 1; i++) {
+            Double acc = calculateAcceleration(pkes.get(i).getValue(), pkes.get(i + 1).getValue(),
+                    pkes.get(i).getTimestamp(), pkes.get(i + 1).getTimestamp());
+            if(acc != null && acc > maxAcc){
+                maxAcc = acc;
+            }
+        }
+
+        return maxAcc;
     }
 
     @Override
--- org.envirocar.app/src/org/envirocar/app/handler/algorithm/MinAccelerationAlgorithm.java
+++ org.envirocar.app/src/org/envirocar/app/handler/algorithm/MinAccelerationAlgorithm.java
@@ -5,11 +5,25 @@ import org.envirocar.obd.events.PropertyKeyEvent;
 
 import java.util.List;
 
-public class MinAccelerationAlgorithm implements DataResponseAlgorithm{
+public class MinAccelerationAlgorithm extends AbstractAccelerationAlgorithm implements DataResponseAlgorithm{
 
     @Override
-    public double calculate(List<PropertyKeyEvent> pke) {
-        return Math.random() * -5;
+    public Double calculate(List<PropertyKeyEvent> pkes) {
+        if(pkes.size() < 2) {
+            return null;
+        }
+        double minAcc = calculateAcceleration(pkes.get(0).getValue(), pkes.get(1).getValue(),
+                pkes.get(0).getTimestamp(), pkes.get(1).getTimestamp());
+
+        for(int i = 1; i < pkes.size() - 1; i++) {
+            Double acc = calculateAcceleration(pkes.get(i).getValue(), pkes.get(i + 1).getValue(),
+                    pkes.get(i).getTimestamp(), pkes.get(i + 1).getTimestamp());
+            if(acc != null && acc < minAcc){
+                minAcc = acc;
+            }
+        }
+
+        return minAcc;
     }
 
     @Override
