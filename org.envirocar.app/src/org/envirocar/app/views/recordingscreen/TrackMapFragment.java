fbb5dbff8da9b6ceb3d3f9e7bd6cd30197d5dd15
Dhiraj Chauhan <Chauhandhiraj40@gmail.com>
2021-11-19 14:50:57 +0530

    changed toast to snackbar

--- org.envirocar.app/src/org/envirocar/app/views/recordingscreen/TrackMapFragment.java
+++ org.envirocar.app/src/org/envirocar/app/views/recordingscreen/TrackMapFragment.java
@@ -31,7 +31,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 
 import com.mapbox.android.core.location.LocationEngineRequest;
 import com.mapbox.android.core.permissions.PermissionsListener;
@@ -61,6 +60,7 @@ import org.envirocar.app.injection.modules.MainActivityModule;
 import org.envirocar.app.R;
 import org.envirocar.app.events.TrackPathOverlayEvent;
 import org.envirocar.app.injection.BaseInjectorFragment;
+import org.envirocar.app.views.obdselection.OBDSelectionFragment;
 import org.envirocar.app.views.trackdetails.MapLayer;
 import org.envirocar.core.logging.Logger;
 
@@ -79,6 +79,13 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
  */
 public class TrackMapFragment extends BaseInjectorFragment implements PermissionsListener {
     private static final Logger LOG = Logger.getLogger(TrackMapFragment.class);
+
+    /**
+     *
+     */
+    public interface ShowSnackbarListener {
+        void showSnackbar(String text);
+    }
 
     private PermissionsManager permissionsManager;
     private MapboxMap mapboxMap;
@@ -330,7 +337,7 @@ public class TrackMapFragment extends BaseInjectorFragment implements Permission
 
     @Override
     public void onExplanationNeeded(List<String> permissionsToExplain) {
-        Toast.makeText(getContext(), "We need access to your location.", Toast.LENGTH_SHORT).show();
+        showSnackbar("We need access to your location.");
     }
 
     @Override
@@ -343,7 +350,7 @@ public class TrackMapFragment extends BaseInjectorFragment implements Permission
                 }
             });
         } else {
-            Toast.makeText(getContext(), "Location access not granted", Toast.LENGTH_LONG).show();
+            showSnackbar("Location access not granted");
         }
     }
 
@@ -397,4 +404,8 @@ public class TrackMapFragment extends BaseInjectorFragment implements Permission
         mMapView.onLowMemory();
     }
 
+    private void showSnackbar(String text) {
+        if (getActivity() instanceof OBDSelectionFragment.ShowSnackbarListener)
+            ((OBDSelectionFragment.ShowSnackbarListener) getActivity()).showSnackbar(text);
+    }
 }
