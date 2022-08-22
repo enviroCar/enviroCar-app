f80f1e3b7db87d77334f2b312da952eb9f16e704
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-08-23 02:24:59 +0530

    added action, activity extras, custom rasa and extra metadata models

---
+++ voicecommand/src/main/java/org/envirocar/voicecommand/model/ExtraMetadata.kt
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2013 - 2022 the enviroCar community
+ *
+ * This file is part of the enviroCar app.
+ *
+ * The enviroCar app is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The enviroCar app is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with the enviroCar app. If not, see http://www.gnu.org/licenses/.
+ */
+package org.envirocar.voicecommand.model
+
+/**
+ * @author Dhiraj Chauhan
+ */
+
+data class ExtraMetadata(
+    val type: String,
+    val recordingMetadata: RecordingMetadata? = null,
+)
+
+data class RecordingMetadata(
+    val recording_status: String,
+    val recording_mode: String,
+    var isDashboardFragment: Boolean,
+    val gps: Boolean,
+    val car: Boolean,
+    val bluetooth: Boolean,
+    val obd_adapter: Boolean,
+    val has_location_permission: Boolean,
+    val has_bluetooth_permission: Boolean,
+)
