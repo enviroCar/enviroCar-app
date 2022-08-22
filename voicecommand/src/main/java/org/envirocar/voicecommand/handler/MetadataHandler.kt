92ea075c0ec40ad29754807ff8bd4ba4a007ebd0
cdhiraj40 <chauhandhiraj40@gmail.com>
2022-08-23 02:22:57 +0530

    Added handler for rasa response and metadata

---
+++ voicecommand/src/main/java/org/envirocar/voicecommand/handler/MetadataHandler.kt
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2013 - 2022 the enviroCar community
+ *
+ * This file is part of the enviroCar app.
+ *
+ * The enviroCar app is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The enviroCar app is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with the enviroCar app. If not, see http://www.gnu.org/licenses/.
+ */
+package org.envirocar.voicecommand.handler
+
+import org.envirocar.voicecommand.model.ExtraMetadata
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * @author Dhiraj Chauhan
+ */
+@Singleton
+class MetadataHandler @Inject constructor() {
+    var metadata: ExtraMetadata? = null
+
+    fun makeIsDashboardFragmentFalse() {
+        metadata?.recordingMetadata?.isDashboardFragment = false
+    }
+
+    fun makeIsDashboardFragmentTrue() {
+        metadata?.recordingMetadata?.isDashboardFragment = true
+    }
+}
