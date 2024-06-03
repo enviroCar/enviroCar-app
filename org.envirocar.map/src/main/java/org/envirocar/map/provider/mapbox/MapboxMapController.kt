f70c33e867a74d8df8f6e7bb2810f6fb3dfe2e83
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-06-03 09:23:48 +0530

    feat: MapboxMapController addPolyline removePolyline

--- org.envirocar.map/src/main/java/org/envirocar/map/provider/mapbox/MapboxMapController.kt
+++ org.envirocar.map/src/main/java/org/envirocar/map/provider/mapbox/MapboxMapController.kt
@@ -2,35 +2,59 @@ package org.envirocar.map.provider.mapbox
 
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.toBitmap
-import com.mapbox.geojson.Point
+import com.mapbox.geojson.Feature
+import com.mapbox.geojson.LineString
 import com.mapbox.maps.CameraBoundsOptions
 import com.mapbox.maps.CameraOptions
 import com.mapbox.maps.EdgeInsets
 import com.mapbox.maps.MapView
+import com.mapbox.maps.extension.style.expressions.dsl.generated.interpolate
+import com.mapbox.maps.extension.style.layers.addLayer
+import com.mapbox.maps.extension.style.layers.generated.lineLayer
+import com.mapbox.maps.extension.style.layers.properties.generated.LineCap
+import com.mapbox.maps.extension.style.layers.properties.generated.LineJoin
+import com.mapbox.maps.extension.style.sources.addSource
+import com.mapbox.maps.extension.style.sources.generated.geoJsonSource
 import com.mapbox.maps.plugin.annotation.annotations
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotation
 import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
-import com.mapbox.maps.plugin.annotation.generated.PolylineAnnotation
 import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
-import com.mapbox.maps.plugin.annotation.generated.createPolylineAnnotationManager
+import kotlinx.coroutines.CompletableDeferred
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import org.envirocar.map.MapController
 import org.envirocar.map.R
 import org.envirocar.map.camera.CameraUpdate
 import org.envirocar.map.model.Marker
+import org.envirocar.map.model.Point
 import org.envirocar.map.model.Polyline
 
 /**
  * [MapboxMapController]
- * -------------------
+ * ---------------------
  * [Mapbox](https://www.mapbox.com) based implementation for [MapController].
  */
 class MapboxMapController(private val viewInstance: MapView) : MapController() {
     // https://docs.mapbox.com/android/maps/guides/annotations/annotations/
+    // https://docs.mapbox.com/android/maps/examples/line-gradient/
+    // NOTE: PolylineAnnotationManager API is not sufficient to create polylines with gradients.
     private val pointAnnotationManager = viewInstance.annotations.createPointAnnotationManager()
-    private val polylineAnnotationManager = viewInstance.annotations.createPolylineAnnotationManager()
 
+    private val scope = CoroutineScope(Dispatchers.Main)
     private val markers = mutableMapOf<Int, PointAnnotation>()
-    private val polylines = mutableMapOf<Int, PolylineAnnotation>()
+    private val polylines = mutableSetOf<Int>()
+    private val styleLoadedCompletableDeferred: CompletableDeferred<Unit> = CompletableDeferred()
+
+    init {
+        if (viewInstance.mapboxMap.style != null) {
+            styleLoadedCompletableDeferred.complete(Unit)
+        } else {
+            viewInstance.mapboxMap.subscribeStyleLoaded {
+                styleLoadedCompletableDeferred.complete(Unit)
+            }
+        }
+    }
 
     override fun setMinZoom(minZoom: Float) {
         super.setMinZoom(minZoom)
@@ -56,7 +80,7 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
             when (this) {
                 is CameraUpdate.Companion.CameraUpdateBasedOnBounds -> {
                     viewInstance.mapboxMap.cameraForCoordinates(
-                        points.map { Point.fromLngLat(it.longitude, it.latitude) },
+                        points.map { it.toMapboxPoint() },
                         CameraOptions.Builder()
                             .padding(padding.toDouble().let { EdgeInsets(it, it, it, it) })
                             .build(),
@@ -71,7 +95,7 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
                 is CameraUpdate.Companion.CameraUpdateBasedOnPoint -> {
                     viewInstance.mapboxMap.setCamera(
                         CameraOptions.Builder()
-                            .center(Point.fromLngLat(point.longitude, point.latitude))
+                            .center(point.toMapboxPoint())
                             .build()
                     )
                 }
@@ -110,7 +134,7 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
         }
         var options = PointAnnotationOptions()
         marker.point.also {
-            options = options.withPoint(Point.fromLngLat(it.longitude, it.latitude))
+            options = options.withPoint(it.toMapboxPoint())
         }
         marker.title?.also {
             options = options.withTextField(it)
@@ -128,7 +152,47 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
         if (polylines.contains(polyline.id)) {
             error("Polyline with ID ${polyline.id} already exists.")
         }
-        // TODO: Missing implementation.
+        scope.launch {
+            styleLoadedCompletableDeferred.await()
+
+            assert(viewInstance.mapboxMap.style != null) {
+                "Mapbox style is not initialized."
+            }
+            polylines.add(polyline.id)
+            viewInstance.mapboxMap.style?.addSource(
+                geoJsonSource(MAPBOX_POLYLINE_SOURCE_ID + polyline.id) {
+                    feature(Feature.fromGeometry(LineString.fromLngLats(polyline.points.map { it.toMapboxPoint() })))
+                    lineMetrics(true)
+                }
+            )
+            viewInstance.mapboxMap.style?.addLayer(
+                lineLayer(
+                    MAPBOX_POLYLINE_LAYER_ID + polyline.id,
+                    MAPBOX_POLYLINE_SOURCE_ID + polyline.id
+                ) {
+                    lineCap(LineCap.ROUND)
+                    lineJoin(LineJoin.ROUND)
+                    lineWidth(polyline.width.toDouble())
+                    lineColor(polyline.color)
+                    lineBorderWidth(polyline.borderWidth.toDouble())
+                    lineBorderColor(polyline.borderColor)
+                    polyline.colors?.also {
+                        lineGradient(
+                            interpolate {
+                                linear()
+                                lineProgress()
+                                for (i in polyline.points.indices) {
+                                    stop {
+                                        literal((i + 1).toDouble() / polyline.points.size.toDouble())
+                                        color(it[i])
+                                    }
+                                }
+                            }
+                        )
+                    }
+                }
+            )
+        }
     }
 
     override fun removeMarker(marker: Marker) {
@@ -142,8 +206,19 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
         if (!polylines.contains(polyline.id)) {
             error("Polyline with ID ${polyline.id} does not exist.")
         }
-        // TODO: Missing implementation.
+        scope.launch {
+            styleLoadedCompletableDeferred.await()
+
+            assert(viewInstance.mapboxMap.style != null) {
+                "Mapbox style is not initialized."
+            }
+            polylines.remove(polyline.id)
+            viewInstance.mapboxMap.style?.removeStyleSource(MAPBOX_POLYLINE_SOURCE_ID + polyline.id)
+            viewInstance.mapboxMap.style?.removeStyleLayer(MAPBOX_POLYLINE_LAYER_ID + polyline.id)
+        }
     }
+
+    private fun Point.toMapboxPoint() = com.mapbox.geojson.Point.fromLngLat(longitude, latitude)
 
     private fun Float.toMapboxBearing() = this
         .times(MAPBOX_CAMERA_BEARING_MAX - MAPBOX_CAMERA_BEARING_MIN)
@@ -168,5 +243,8 @@ class MapboxMapController(private val viewInstance: MapView) : MapController() {
         internal const val MAPBOX_CAMERA_TILT_MAX = 60.0F
         internal const val MAPBOX_CAMERA_ZOOM_MIN = 0.0F
         internal const val MAPBOX_CAMERA_ZOOM_MAX = 22.0F
+
+        internal const val MAPBOX_POLYLINE_LAYER_ID = "polyline-layer-"
+        internal const val MAPBOX_POLYLINE_SOURCE_ID = "polyline-source-"
     }
 }
