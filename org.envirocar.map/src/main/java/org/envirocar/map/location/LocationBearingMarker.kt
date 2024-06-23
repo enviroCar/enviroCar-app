a09273a7c536b583e1d626a5065a090be23fd077
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-06-23 21:38:59 +0530

    refactor: bitmap in LocationBearingMarker & LocationPointMarker

--- org.envirocar.map/src/main/java/org/envirocar/map/location/LocationBearingMarker.kt
+++ org.envirocar.map/src/main/java/org/envirocar/map/location/LocationBearingMarker.kt
@@ -1,5 +1,9 @@
 package org.envirocar.map.location
 
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import org.envirocar.map.R
 import org.envirocar.map.model.Marker
 import org.envirocar.map.model.Point
@@ -9,13 +13,32 @@ import org.envirocar.map.model.Point
  *  -------------------------
  *  The [Marker] used to display the current location's bearing.
  */
-internal class LocationBearingMarker(point: Point, bearing: Float) :
-    Marker(ID, point, TITLE, DRAWABLE, SCALE, bearing) {
+internal class LocationBearingMarker(
+    point: Point,
+    bearing: Float,
+    context: Context
+) : Marker(
+    ID,
+    point,
+    TITLE,
+    DRAWABLE,
+    bitmap ?: synchronized(lock) {
+         bitmap ?: AppCompatResources.getDrawable(context, R.drawable.location_bearing)!!.toBitmap()
+             .also { bitmap = it }
+    },
+    SCALE,
+    bearing
+) {
 
     companion object {
+        private val lock = Any()
+
+        @Volatile
+        private var bitmap: Bitmap? = null
+
         private const val ID = -0xBL
         private val TITLE = null
-        private val DRAWABLE = R.drawable.location_bearing
+        private val DRAWABLE = null
         private const val SCALE = 0.2F
     }
 }
--- org.envirocar.map/src/main/java/org/envirocar/map/location/LocationPointMarker.kt
+++ org.envirocar.map/src/main/java/org/envirocar/map/location/LocationPointMarker.kt
@@ -1,5 +1,9 @@
 package org.envirocar.map.location
 
+import android.content.Context
+import android.graphics.Bitmap
+import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.graphics.drawable.toBitmap
 import org.envirocar.map.R
 import org.envirocar.map.model.Marker
 import org.envirocar.map.model.Point
@@ -9,13 +13,31 @@ import org.envirocar.map.model.Point
  *  -------------------------
  *  The [Marker] used to display the current location.
  */
-internal class LocationPointMarker(point: Point) :
-    Marker(ID, point, TITLE, DRAWABLE, SCALE, ROTATION) {
+internal class LocationPointMarker(
+    point: Point,
+    context: Context
+) : Marker(
+    ID,
+    point,
+    TITLE,
+    DRAWABLE,
+    bitmap ?: synchronized(lock) {
+        bitmap ?: AppCompatResources.getDrawable(context, R.drawable.location_point)!!.toBitmap()
+            .also { bitmap = it }
+    },
+    SCALE,
+    ROTATION
+) {
 
     companion object {
+        private val lock = Any()
+
+        @Volatile
+        private var bitmap: Bitmap? = null
+
         private const val ID = -0xCL
         private val TITLE = null
-        private val DRAWABLE = R.drawable.location_point
+        private val DRAWABLE = null
         private const val SCALE = 0.18F
         private const val ROTATION = 0.0F
     }
