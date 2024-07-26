9eb7d086907febbb41bec3927f7d6a974e2c0a77
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-07-27 01:23:21 +0530

    feat: MapLibreMapProvider & MapLibreMapController

---
+++ org.envirocar.map/src/main/java/org/envirocar/map/provider/maplibre/MapLibreMapProvider.kt
@@ -0,0 +1,60 @@
+package org.envirocar.map.provider.maplibre
+
+import android.content.Context
+import org.envirocar.map.MapController
+import org.envirocar.map.MapProvider
+import org.maplibre.android.MapLibre
+import org.maplibre.android.maps.MapView
+
+/**
+ * [MapLibreMapProvider]
+ * ---------------------
+ * [MapLibre](https://maplibre.org) based implementation for [MapProvider].
+ *
+ * Following options are available to be configured:
+ *
+ * @param style
+ * The style for the map to be loaded from a specified URI or from a JSON represented as [String].
+ * The URI can be one of the following forms:
+ * * `http://`
+ * * `https://`
+ * * `asset://`
+ * * `file://`
+ */
+class MapLibreMapProvider(
+    private val style: String
+) : MapProvider {
+    private lateinit var viewInstance: MapView
+    private lateinit var controllerInstance: MapLibreMapController
+
+    override fun getView(context: Context): MapView {
+        if (!initailized) {
+            initailized = true
+            // Must be called before consuming any MapLibre API.
+            MapLibre.getInstance(context)
+        }
+        if (!::viewInstance.isInitialized) {
+            viewInstance = MapView(context).apply {
+                getMapAsync {
+                    it.setStyle(style)
+                }
+            }
+        }
+        return viewInstance
+    }
+
+    override fun getController(): MapController {
+        if (!::viewInstance.isInitialized) {
+            error("MapLibreMapProvider is not initialized.")
+        }
+        if (!::controllerInstance.isInitialized) {
+            controllerInstance = MapLibreMapController(viewInstance)
+        }
+        return controllerInstance
+    }
+
+    companion object {
+        @Volatile
+        private var initailized = false
+    }
+}
