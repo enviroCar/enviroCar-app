895d51552ef1bb3a7e5f84d8f6f906d614c0f9b8
Hitesh Kumar Saini <saini123hitesh@gmail.com>
2024-07-27 14:39:39 +0530

    feat: build option for map providers

--- org.envirocar.map/build.gradle.kts
+++ org.envirocar.map/build.gradle.kts
@@ -1,3 +1,9 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val enableMapbox = gradleLocalProperties(rootDir, providers).getOrDefault("org.envirocar.map.enableMapbox", "true") == "true"
+val enableMapLibre = gradleLocalProperties(rootDir, providers).getOrDefault("org.envirocar.map.enableMapLibre", "true") == "true"
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.jetbrains.kotlin.android)
@@ -38,9 +44,23 @@ dependencies {
     androidTestImplementation(libs.androidx.espresso.core)
 
     // Provider: Mapbox
-    implementation(libs.mapbox.maps.android)
+    if (enableMapbox) {
+        implementation(libs.mapbox.maps.android)
+    }
 
     // Provider: MapLibre
-    implementation(libs.maplibre.gl.android.sdk)
-    implementation(libs.maplibre.gl.android.plugin.annotation.v9)
+    if (enableMapLibre) {
+        implementation(libs.maplibre.gl.android.sdk)
+        implementation(libs.maplibre.gl.android.plugin.annotation.v9)
+    }
+}
+
+// https://stackoverflow.com/a/69141612/12825435
+tasks.withType<KotlinCompile>().configureEach {
+    if (!enableMapbox) {
+        exclude("**/provider/mapbox/**.kt")
+    }
+    if (!enableMapLibre) {
+        exclude("**/provider/maplibre/**.kt")
+    }
 }
